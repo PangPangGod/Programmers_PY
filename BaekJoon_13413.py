41fc573f8ed552f7dc47cb5118f343b81d5439f6
PangPangGod <rhcp1134@gmail.com>
2022-03-22 18:14:47 +0900

    Solved in 2022-03-22

---
+++ BaekJoon_13413.py
@@ -0,0 +1,31 @@
+#오셀로 재배치 13413
+#https://www.acmicpc.net/problem/13413
+
+import sys
+
+read = sys.stdin.readline
+
+T = int(read().rstrip())
+
+answers = []
+result = 0
+arr = []
+
+for _ in range(T):
+    N = int(read().rstrip())
+    start = list(read().rstrip())
+    goal = list(read().rstrip())
+
+    for i in range(N):
+        if start[i] != goal[i]:
+            arr.append(start[i])
+
+    if arr.count("B") >= arr.count("W"):
+        result = arr.count("B")
+    else:
+        result = arr.count("W")
+    answers.append(result)
+    arr = []
+
+for answer in answers:
+    print(answer)
